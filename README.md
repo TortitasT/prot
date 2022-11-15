1f3e8c9b3e663acc5e40e8045ac34be6c2c4cb85
Tortitas <victorgf2011@gmail.com>
2022-11-15 14:17:34 +0100

    multi platform support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # prot
 
-Project generator made in Denoland.
+Project generator made in Deno.
 
 ## Usage
 
@@ -10,17 +10,36 @@ prot new <name> [template]
 
 ## Install
 
-Requires Denoland to be installed.
+Requires Deno to be installed.
 ```powershell
 git clone https://github.com/tortitast/prot.git
 cd prot
-deno install --allow-env --allow-read --allow-write -f .\main.ts
+deno install --allow-env --allow-read --allow-write -f main.ts
 prot sync
+```
+
+### !!! On mac
+
+Add this line to your shell .rc file (e.g. ~/.bashrc, ~/.zshrc, ~/.config/fish/config.fish)
+```bash
+export PATH="/Users/vgf/.deno/bin:$PATH"
 ```
 
 ## Templates
 
 To add a template create a folder with the name of the template and the files inside.
+
+- Windows
 ```powershell
 %USERPROFILE%\AppData\Roaming\Tortitas\prot\templates
+```
+
+- Linux
+```bash
+~/.config/tortitas/prot/templates
+```
+
+- Mac
+```bash
+~/Library/Application Support/Tortitas/prot/templates
 ```
--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 import { Command, copy, ensureDir, exists } from "./deps.ts";
 
-const TEMPLATES_DIR = `${
-  Deno.env.get("USERPROFILE")
-}\\AppData\\Roaming\\Tortitas\\prot\\templates`;
+const ALL_TEMPLATES_DIR = {
+  windows: `${
+    Deno.env.get("USERPROFILE")
+  }\\AppData\\Roaming\\Tortitas\\prot\\templates`,
+
+  linux: `${Deno.env.get("HOME")}/.config/tortitas/prot/templates`,
+
+  darwin: `${
+    Deno.env.get("HOME")
+  }/Library/Application Support/Tortitas/prot/templates`,
+};
+
+const TEMPLATES_DIR = ALL_TEMPLATES_DIR[Deno.build.os];
 
 const ASCII_TEAPOT = `%c
               ;,'
@@ -12,17 +22,6 @@ const ASCII_TEAPOT = `%c
  \`-\     /
     \`-=-'     hjw
 `;
-
-// if (!(await exists(TEMPLATES_DIR))) {
-//   await ensureDir(TEMPLATES_DIR);
-
-//   console.log("%cInstalling templates...", "color: blue");
-
-//   await copy(
-//     "./templates",
-//     TEMPLATES_DIR,
-//   );
-// }
 
 if (import.meta.main) {
   await new Command()
@@ -53,38 +52,48 @@ if (import.meta.main) {
       new Command()
         .description("Create a new project")
         .arguments("<name:string> [template:string]")
-        .action(async (_options, name: string, template?: string, ...args) => {
-          if (await exists(name)) {
-            console.error(`❌ %cDirectory ${name} already exists`, "color: red");
-            Deno.exit(1);
-          }
+        .action(
+          async (
+            _options: any,
+            name: string,
+            template?: string,
+            ...args: any
+          ) => {
+            if (await exists(name)) {
+              console.error(
+                `❌ %cDirectory ${name} already exists`,
+                "color: red",
+              );
+              Deno.exit(1);
+            }
 
-          const selectedTemplate = template || "default";
+            const selectedTemplate = template || "default";
 
-          if (!await exists(`${TEMPLATES_DIR}/${selectedTemplate}`)) {
-            console.error(
-              `❌ %cTemplate ${selectedTemplate} does not exist`,
-              "color: red",
+            if (!await exists(`${TEMPLATES_DIR}/${selectedTemplate}`)) {
+              console.error(
+                `❌ %cTemplate ${selectedTemplate} does not exist`,
+                "color: red",
+              );
+              Deno.exit(1);
+            }
+
+            await Deno.mkdirSync(name);
+
+            await copy(
+              `${TEMPLATES_DIR}/${selectedTemplate}`,
+              `${name}`,
+              { overwrite: true },
             );
-            Deno.exit(1);
-          }
 
-          await Deno.mkdirSync(name);
+            console.info(ASCII_TEAPOT, "color: blue");
+            console.info(
+              `✅ %cProject ${name} successfully created with template ${selectedTemplate}`,
+              "color: green;",
+            );
 
-          await copy(
-            `${TEMPLATES_DIR}/${selectedTemplate}`,
-            `${name}`,
-            { overwrite: true },
-          );
-
-          console.info(ASCII_TEAPOT, "color: blue");
-          console.info(
-            `✅ %cProject ${name} successfully created with template ${selectedTemplate}`,
-            "color: green;",
-          );
-
-          Deno.exit(0);
-        }),
+            Deno.exit(0);
+          },
+        ),
     )
     .parse(Deno.args);
 }
