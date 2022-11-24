c79c950c67aeea91e5bef71c79c3806663ec3a42
TortitasT <victorgf2011@gmail.com>
2022-11-24 22:26:40 +0100

    add comment

--- templates/java/Program.java
+++ templates/java/Program.java
@@ -5,6 +5,14 @@ public class Program {
         String name = Utility.Prompt("What is your name?", String.class);
         int age = Utility.Prompt("What is your age?", Integer.class);
 
-        System.out.println("Hello " + name + ", you are " + age + " years old.");
+        // Print the user's name and age
+
+            System.out.println("Hello " + name + ", you are " + age + " years old.");
     }
 }
+
+
+
+
+
+
